cd0714c71a621eacf071d21c9b5f209ce3c83e91
Onoja Victor <onoja.jsdev@gmail.com>
2023-02-05 17:32:29 +0100

    Add route to get all categories and single category

---
+++ routes/categories.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const categories_model = require("../models/categories") 
+const router = express.Router()
+
+//GET ALL categories
+router.get("/", async (req, res) => {
+    const categories = await categories_model.find().lean()
+    res.send(categories)
+})
+
+
+//GET category SINGLE category
+
+router.get("/category", async (req, res) => { categories_model
+    const { category_id } = req.body
+    const category = await categories_model.findOne({ _id: category_id })
+    res.send(category)
+})
+
+
+
+
+
+module.exports = router
+
+
+//NOTES
+// We should also be able to get all categories for a cinema, theater and vendor
